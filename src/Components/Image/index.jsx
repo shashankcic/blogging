23830ccf24f6d556fa574a15170004bf2940bfd1
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-09-20 08:58:56 +0000

    adding image functionality

---
+++ src/Components/Image/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = ({src, alt}) => (
+  <div>
+    <img src={src} alt={alt} />
+  </div>
+);
+
+export default Image;
--- src/Components/Item/index.jsx
+++ src/Components/Item/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { ItemWrapper }  from './style';
+import Image from '../Image';
+
+export const componentMappings = {
+  img: (content) => <Image {...content} />
+};
 
 const Item = ({ type, content, updateItem, handleKeyPress }) => {
   const textBox = React.useRef();
@@ -18,7 +23,7 @@ const Item = ({ type, content, updateItem, handleKeyPress }) => {
             value={content}
             onChange = {(e) => updateItem(e.target.value)}
           />
-        ) : <p>Some new item</p>
+        ) : componentMappings[type](content)
       }
     </ItemWrapper>
   );
